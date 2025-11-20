b7fa297d14d1af04be1fcf45559bf00d6b891a31
AbeerOsman <abeerjilani90@gmail.com>
2025-11-20 10:26:26 +0300

    vertion 0.2

---
+++ Challenge3Team3/Views/Registration.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Challenge3Team3
+//
+//  Created by Abeer Jeilani Osman  on 29/05/1447 AH.
+//
+
+import SwiftUI
+
+struct Registration: View {
+    var body: some View {
+        VStack {
+            //new
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Registration()
+}
