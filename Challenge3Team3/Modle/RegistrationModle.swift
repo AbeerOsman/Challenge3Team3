99ae51a3ec0cec0f79ee1524f8159ad5e975f3b9
AbeerOsman <abeerjilani90@gmail.com>
2025-11-20 10:30:32 +0300

    vestion 0.3

---
+++ Challenge3Team3/Modle/RegistrationModle.swift
@@ -0,0 +1,8 @@
+//
+//  Registration.swift
+//  Challenge3Team3
+//
+//  Created by Abeer Jeilani Osman  on 29/05/1447 AH.
+//
+//
+import Foundation
---
+++ Challenge3Team3/ViewModles/RegistrationViewModle.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewModle.swift
+//  Challenge3Team3
+//
+//  Created by Abeer Jeilani Osman  on 29/05/1447 AH.
+//
+//
+import Foundation
